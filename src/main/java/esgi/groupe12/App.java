b798cd7ce154d02e9e7cfa5bf6968a980a05ebb3
tbriand <briandthibaud99@gmail.com>
2022-03-03 15:15:19 +0100

    [Update] Update Main

--- src/main/java/esgi/groupe12/App.java
+++ src/main/java/esgi/groupe12/App.java
@@ -6,20 +6,16 @@ import esgi.groupe12.core.tools.parser.OcrParser;
 import esgi.groupe12.core.tools.fileManagement.CreateFile;
 import esgi.groupe12.core.tools.fileManagement.ReadFile;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         ReadFile readFile = new ReadFile(
-                "/home/thibaud-briand/Programe/java_/KattaImpovedOCR/fileInput/",
+                "/home/thibaud-briand/Programme/java_/KattaImpovedOCR/fileInput/",
                 100 * 4
         );
         CreateFile createFile = new CreateFile(
-                "/home/thibaud-briand/Programe/java_/KattaImpovedOCR/fileOutput/"
+                "/home/thibaud-briand/Programme/java_/KattaImpovedOCR/fileOutput/"
         );
 
         OcrParser ocrParser = new OcrParser(4, 3);
